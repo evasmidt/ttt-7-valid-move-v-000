52905e6475af65317cfcce0d69e3fd381035a35b
Eva Smidt <shevangeline@gmail.com>
2018-01-28 22:58:12 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,7 +7,7 @@
 #end
 
 def valid_move?(board, index)
- index.between?(0, 8) && position_taken? == false
+ index.between?(0, 8) && position_taken?(board, index) == false
 
 
 #position_taken? == true
